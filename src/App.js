600dce7fdec77579962e21fd3308a49d4be67756
Jafango <jfrogg.au@gmail.com>
2024-09-26 21:43:46 +0930

    check

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ function App() {
         <Card.Body>
           <Card.Title> Card Title </Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Some wow test
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
