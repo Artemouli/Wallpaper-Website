44b88101be662f1516973fcc8531d465ec1fa162
Jafango <jfrogg.au@gmail.com>
2024-09-26 22:10:56 +0930

    React-Bootstrap working, first card test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Placeholder from 'react-bootstrap/Placeholder';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="d-flex justify-content-around">
-      <Card style={{ width: '18rem'}}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title> Card Title </Card.Title>
-          <Card.Text>
-            Some wow test
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+    <div className="App">
+      <div className='mt-5'>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
